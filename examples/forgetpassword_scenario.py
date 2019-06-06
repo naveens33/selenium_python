26430da521aa643206ff98d2d95a057a58bcb3d8
NaveenS <naveensagayaselvaraj@gmail.com>
2019-06-06 21:07:50 +0530

    Update forgetpassword_scenario.py

--- examples/forgetpassword_scenario.py
+++ examples/forgetpassword_scenario.py
@@ -5,7 +5,7 @@ driver.maximize_window()
 driver.get("http://zero.webappsecurity.com/index.html")
 driver.find_element_by_id("signin_button").click()
 assert "Zero - Log in"==driver.title
-driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div/div/a").click()
+driver.find_element_by_partial_link_text("Forgot").click()
 driver.find_element_by_xpath("//*[@id='user_email']").send_keys("abc@gmail.com")
 driver.find_element_by_xpath("//*[@id='send_password_form']/div[2]/input").click()
 
