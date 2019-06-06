99e38e8d1777f0d7722bc63a3e381669ab892ed5
Naveen.S <michealtina919>
2019-06-06 21:05:48 +0530

    minor change

--- examples/login_scenario.py
+++ examples/login_scenario.py
@@ -6,7 +6,6 @@ driver.maximize_window()
 driver.get("http://zero.webappsecurity.com/")
 signin=driver.find_element_by_id("signin_button")
 signin.click()
-
 driver.find_element_by_id("user_login").send_keys("username")
 driver.find_element_by_id("user_password").send_keys("password")
 driver.find_element_by_name("submit").click()
