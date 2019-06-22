e892a741c64eeff32534cb9e902bbc8aeeb1e79a
Naveen.S <michealtina919>
2019-06-22 12:13:09 +0530

    screenshots

--- examples/login_scenario.py
+++ examples/login_scenario.py
@@ -1,16 +1,29 @@
 from selenium import webdriver
 
-driver =webdriver.Chrome(r'D:\Naveen\Selenium\chromedriver_win32\chromedriver.exe')
+def verify_text(expected,actual):
+    if expected!=actual:
+        print("Verification failed: ",expected,actual)
+
+driver =webdriver.Chrome(r'D:/Naveen/Selenium/chromedriver_win32/chromedriver.exe')
+
 driver.maximize_window()
 
 driver.get("http://zero.webappsecurity.com/")
+
 signin=driver.find_element_by_id("signin_button")
+
 signin.click()
+
+assert "Zero - Log in" ==driver.title
+
+#verify_text("Zero - Log in",driver.title)
+
 driver.find_element_by_id("user_login").send_keys("username")
+
 driver.find_element_by_id("user_password").send_keys("password")
+
 driver.find_element_by_name("submit").click()
 
-assert "Zero - Account Summary"==driver.title
-
 #driver.close()
-#driver.quit()
+
+driver.quit()
