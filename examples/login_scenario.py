22c290b655d039e63e5199247502b26a5d808c14
Naveen.S <michealtina919>
2019-07-02 07:37:42 +0530

    verify function added

--- examples/login_scenario.py
+++ examples/login_scenario.py
@@ -1,29 +1,21 @@
 from selenium import webdriver
 
-def verify_text(expected,actual):
+def Verify(expected,actual,message):
     if expected!=actual:
-        print("Verification failed: ",expected,actual)
+        print(message)
 
-driver =webdriver.Chrome(r'D:/Naveen/Selenium/chromedriver_win32/chromedriver.exe')
 
+driver=webdriver.Chrome(r"D:/Naveen/Selenium/chromedriver_win32/chromedriver.exe")
 driver.maximize_window()
-
-driver.get("http://zero.webappsecurity.com/")
-
+driver.get("http://zero.webappsecurity.com")
 signin=driver.find_element_by_id("signin_button")
-
 signin.click()
-
-assert "Zero - Log in" ==driver.title
-
-#verify_text("Zero - Log in",driver.title)
-
+assert "Zero - Log in"==driver.title
+signin2=driver.find_element_by_name("submit")
+Verify(False,signin2.is_enabled(),"Signin button is not disabled")
 driver.find_element_by_id("user_login").send_keys("username")
-
 driver.find_element_by_id("user_password").send_keys("password")
-
-driver.find_element_by_name("submit").click()
-
+signin2.click()
+assert  "Zero - Account Summary"==driver.title
 #driver.close()
-
-driver.quit()
+#driver.quit()
