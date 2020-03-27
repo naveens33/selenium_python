40cd328c2606b848c646eddd3e35c31ae65781af
Naveen.S <naveensagayaselvaraj@gmail.com>
2020-03-27 07:36:28 +0530

    dropdown image added

--- different_browser/firefox_example.py
+++ different_browser/firefox_example.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
-driver=webdriver.Firefox(executable_path=r'D:/Naveen/Selenium/geckodriver-v0.22.0-win64/geckodriver.exe')
+driver=webdriver.Firefox(executable_path=r'D:\Naveen\Selenium\geckodriver-v0.26.0-win64/geckodriver.exe')
 driver.maximize_window()
 driver.get("http://zero.webappsecurity.com/index.html")
 driver.quit()
